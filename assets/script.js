5e0cdac9af624f1bf5543dd0a0b406c08b2b36df
Margaux Burban <burban.margaux@gmail.com>
2024-06-06 14:26:45 +0200

    Ajout des bullet points et essai de création du carrousel

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,39 @@
-const slides = [
+/*const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
+];
+*/
+
+const slides = [
+	"./assets/images/slideshow/slide1.jpg", 
+	"slideshow/slide2.jpg",
+	"./assets/images/slideshow/slide3.jpg",
+	"./assets/images/slideshow/slide4.png",
 ];
 
 const arrowLeft = document.getElementById("arrow_left");
 const arrowRight = document.getElementById("arrow_right");
+let bannerImage = document.getElementById("banner_image")
 
-function arrowClick(direction) {
-	if (direction === "left") {
-		alert("Clic flèche de gauche")
+/*function arrowClick(direction) {
+	if (direction === "right") {
+		alert("précédent")
 	} else {
-		alert("Clic flèche de droite")
+		alert("suivant")
 	}
 };
 
@@ -34,4 +43,17 @@ arrowLeft.addEventListener("click", function() {
 
 arrowRight.addEventListener("click", function() {
 	arrowClick("right")
+});
+*/
+
+function changeSlide() {
+	document.getElementById("banner_image").src = "images/" + slides[2];
+};
+
+
+arrowLeft.addEventListener("click", function() {
+	changeSlide(-1);
+});
+arrowRight.addEventListener("click", function() {
+	changeSlide(1);
 });
